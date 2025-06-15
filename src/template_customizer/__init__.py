55b3460ca0033d1c1f2958d651d5f9d999dfe7f8
mkuhl <mkuhl@softmachine.at>
2025-06-15 10:16:34 +0000

    Bump version to 0.1.3

--- src/template_customizer/__init__.py
+++ src/template_customizer/__init__.py
@@ -1,5 +1,5 @@
 """Template Customizer - Comment-based template processing tool."""
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Template Customizer"
 __description__ = "A tool for customizing project templates using comment-based markers"
